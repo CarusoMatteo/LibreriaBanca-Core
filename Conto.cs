6f6a8e8c502250635c8a746fa6a7413bd33f21b6
CarusoMatteo <matteo.caruso.scuola@gmail.com>
2022-04-30 09:38:15 +0200

    Aggiungere i file di progetto.

---
+++ Conto.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaBanca
+{
+    /// <summary>
+    /// Uno dei Conti dell'utente.
+    /// </summary>
+    public class Conto
+    {
+        string nomeConto, passwordConto, tipoMoneta, tipoConto, chiaveConto;
+        double saldo;
+
+        /// <summary>
+        /// Crea un Conto di un profilo.
+        /// </summary>
+        /// <param name="nomeConto">Il nome del Conto.</param>
+        /// <param name="passwordConto">La password del Conto.</param>
+        /// <param name="tipoMoneta">Il tipo di moneta usata nel conto.</param>
+        /// <param name="tipoConto">Il tipo di conto.</param>
+        /// <param name="chiaveConto">La chiave del conto.</param>
+        public Conto(string nomeConto, string passwordConto, string tipoMoneta, string tipoConto, string chiaveConto)
+        {
+            this.nomeConto = nomeConto;
+            this.passwordConto = passwordConto;
+            this.tipoMoneta = tipoMoneta;
+            this.tipoConto = tipoConto;
+            this.chiaveConto = chiaveConto;            
+        }
+
+        /// <summary>
+        /// Crea un Conto di un profilo.
+        /// <para/> La moneta predefinità è l'euro (EUR).
+        /// </summary>
+        /// <param name="nomeConto">Il nome del Conto.</param>
+        /// <param name="passwordConto">La password per accedere al Conto.</param>
+        /// <param name="tipoConto">Il tipo di conto.</param>
+        /// <param name="chiaveConto">La chiave del conto.</param>
+        public Conto(string nomeConto, string passwordConto, string tipoConto, string chiaveConto)
+        {
+            this.nomeConto = nomeConto;
+            this.passwordConto = passwordConto;
+            tipoMoneta = "EUR";
+            this.tipoConto = tipoConto;
+            this.chiaveConto = chiaveConto;
+        }
+        
+        /// <summary>
+        /// Restituisce la quantità di soldi associati al conto.
+        /// </summary>
+        public double Saldo { get => saldo; }
+
+        /// <summary>
+        /// Restituisce il nome del Conto.
+        /// </summary>
+        public string NomeConto { get => nomeConto; }
+
+        /// <summary>
+        /// Restituisce la password del Conto.
+        /// </summary>
+        public string PasswordConto { get => passwordConto; }
+
+        /// <summary>
+        /// Restituisce il tipo di moneta usata nel conto.
+        /// </summary>
+        public string TipoMoneta { get => tipoMoneta; }
+
+        /// <summary>
+        /// Restituisce il tipo di conto.
+        /// </summary>
+        public string TipoConto { get => tipoConto; }
+
+        /// <summary>
+        /// Restituisce la chiave del conto.
+        /// </summary>
+        public string ChiaveConto { get => chiaveConto; }
+
+        /// <summary>
+        /// Sottrae dal saldo del conto la quantità.
+        /// <para/> Restituisce un booleano in base a se la transazione è avvenuta con successo.
+        /// </summary>
+        /// <param name="quantità">La quantità di soldi da prelevare.</param>
+        /// <returns>Se la transazione ha avuto successo.</returns>
+        public bool Prelievo(int quantità)
+        {
+            if (saldo >= quantità)
+            {
+                saldo -= quantità;
+                return true;
+            }
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// Aggiunge al saldo del conto la quantità.
+        /// </summary>
+        /// <param name="quantità">La quantità di soldi da prelevare.</param>
+        public void Deposito(int quantità)
+        {
+            saldo += quantità;
+        }
+    }
+}
---
+++ ProfiloUtente.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaBanca
+{
+    /// <summary>
+    /// Il profilo dell'utente.
+    /// </summary>
+    public class ProfiloUtente
+    {
+        string nome, cognome, email, dataNascita, luogoNascita;
+        string password;
+        List<Conto> listaConti;
+
+        /// <summary>
+        /// Crea un Profilo Utente
+        /// </summary>
+        /// <param name="nome">Il nome del profilo.</param>
+        /// <param name="cognome">Il cognome del profilo.</param>
+        /// <param name="email">L'indirizzo e-mail associato al profilo.</param>
+        /// <param name="dataNascita">La data di nascita.</param>
+        /// <param name="luogoNascita">Il luogo di nascita.</param>
+        /// <param name="password">La password usata per accedere al profilo.</param>
+        public ProfiloUtente(string nome, string cognome, string email, string dataNascita, string luogoNascita, string password)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.email = email;
+            this.dataNascita = dataNascita;
+            this.luogoNascita = luogoNascita;
+
+            this.password = password;
+
+            listaConti = new List<Conto>();
+        }
+
+
+        /// <summary>
+        /// Restituisce il nome del profilo.
+        /// </summary>
+        public string Nome { get => nome; }
+
+        /// <summary>
+        /// Restituisce il cognome del profilo.
+        /// </summary>
+        public string Cognome { get => cognome; }
+
+        /// <summary>
+        /// Restituisce l'indirizzo e-mail associato al profilo.
+        /// <para/> Dev'essere usato per accedere al profilo.
+        /// </summary>
+        public string Email { get => email; }
+
+        /// <summary>
+        /// Restituisce la data di nascita.
+        /// </summary>
+        public string DataNascita { get => dataNascita; }
+
+        /// <summary>
+        /// Restituisce la data di nascita.
+        /// </summary>
+        public string LuogoNascita { get => luogoNascita; }
+
+        /// <summary>
+        /// Restituisce la password usata per accedere al profilo.
+        /// <para/> Dev'essere usato per accedere al profilo.
+        /// </summary>
+        public string Password { get => password; }
+
+        /// <summary>
+        /// Restituisce la Lista di Conti.
+        /// </summary>
+        public List<Conto> ListaConti { get => listaConti; }
+
+        /// <summary>
+        /// Aggiunge un nuovo conto alla Lista di Conti.
+        /// </summary>
+        /// <param name="c">Il Conto da aggiungere.</param>
+        public void AggiungiConto(Conto c)
+        {
+            listaConti.Add(c);
+        }        
+    }
+}
